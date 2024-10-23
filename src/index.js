07e8eac08d7851ff0e04b3f9da926afa98fe9dbb
Taofeeq Olatigbe <olatigbetaofeeq@gmail.com>
2024-10-23 01:25:20 +0100

    day 1 of learning react

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDom from 'react-dom';
+
+const App = () => {
+    return (
+        <div>hi there!</div>
+    )
+}
+
+ReactDom.render(
+    <App/>,
+    document.querySelector('#root')
+)
