74b0cb836bf55e20399230fc3958a1e4d4cf24fb
Taofeeq Olatigbe <olatigbetaofeeq@gmail.com>
2024-10-24 23:02:13 +0100

    End of Day 2

--- src/index.js
+++ src/index.js
@@ -1,73 +1,33 @@
 import React from "react";
 import ReactDom from "react-dom";
+import SingleComment from "./singleComment";
 import img1 from "./images/member-1.jpg";
 import img2 from "./images/member-2.jpg";
 import img3 from "./images/member-3.jpg";
 
 const App = () => {
+  return (
+    <div className="ui comments">
+      <SingleComment
+            name="jake"
+            image={img1}
+            date="Today at 5:00PM"
+            text="It's Amazing"
+      />
+      <SingleComment
+            name="sarah"
+            image={img2}
+            date="Today at 3:00PM"
+            text="It's okay"
+      />
+      <SingleComment
+            name="mike"
+            image={img3}
+            date="Today at 2:48PM"
+            text="Just what i Wanted"
+      />
+    </div>
+  );
+};
 
-    return (
-        <div className="ui comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img src={img1} alt="profile"/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        sarah 
-                    </a>
-                    <div className="metadata">
-                        <span className="date">
-                            Today at 5:00PM
-                        </span>
-                    </div>
-                    <div className="text">
-                        it's amazing
-                    </div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img src={img2} alt="profile"/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        sarah 
-                    </a>
-                    <div className="metadata">
-                        <span className="date">
-                            Today at 5:00PM
-                        </span>
-                    </div>
-                    <div className="text">
-                        it's amazing
-                    </div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img src={img3} alt="profile"/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        sarah 
-                    </a>
-                    <div className="metadata">
-                        <span className="date">
-                            Today at 5:00PM
-                        </span>
-                    </div>
-                    <div className="text">
-                        it's amazing
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-
-}
-
-ReactDom.render(
-    <App/>,
-    document.querySelector('#root')
-)
+ReactDom.render(<App />, document.querySelector("#root"));
